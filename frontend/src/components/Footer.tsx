e76e78a499394dd25a511b2ec6acbfdd1526c6d3
Jethro Djan <jthryeboah@gmail.com>
2025-10-04 16:07:04 +0000

    Remove nav items from Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,14 @@
 function Footer() {
     return (
         <footer className="border-t border-border-light text-center px-6 py-8">
-            <nav className="flex flex-col md:flex-row gap-4 md:gap-8 justify-center mb-6">
+        {/* <nav className="flex flex-col md:flex-row gap-4 md:gap-8 justify-center mb-6">
                 <a href='#'className='font-sans text-sm text-primary-light'>Home</a>
                 <a href='#'className='font-sans text-sm text-primary-light'>Essays</a>
                 <a href='#'className='font-sans text-sm text-primary-light'>Authors</a>
                 <a href='#'className='font-sans text-sm text-primary-light'>About</a>
                 <a href='#'className='font-sans text-sm text-primary-light'>Privacy</a>
                 <a href='#'className='font-sans text-sm text-primary-light'>Contact</a>
-            </nav>
+            </nav> */}
             <p className="font-sans text-xs italic text-primary-light">&copy; 2025 The Oblivion Times. All thoughts reserved.</p>
         </footer>
     );
