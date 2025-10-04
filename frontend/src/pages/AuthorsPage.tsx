806e69bf1d24cae0172838f335f20bef33866642
Jethro Djan <jthryeboah@gmail.com>
2025-10-04 16:10:11 +0000

    Add some html

--- frontend/src/pages/AuthorsPage.tsx
+++ frontend/src/pages/AuthorsPage.tsx
@@ -1,6 +1,14 @@
 const AuthorsPage = () => {
     return (
         <>
+            <section className="px-4 py-12">
+                <div className="max-w-4xl mx-auto text-center">
+                    <h1 className="font-serif text-3xl md:text-4xl font-semibold mb-6">Oblivion Times</h1>
+                    <p className="font-serif text-2xl italic text-primary-light mb-8 leading-relaxed">
+                        The hub for the refined thoughts of wondering minds.
+                    </p>
+                </div>
+            </section>
         </>
     );
 }
